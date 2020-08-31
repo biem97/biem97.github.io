f4dc449973e9d7129f7e8611a565fbf9ab98972e
Son Nguyen <nyenson1997@gmail.com>
2020-08-31 00:11:35 -0500

    Updates

---
+++ precache-manifest.d9d84fef8cebf54951558a67e17b7bfe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645826dc9fe882891b055632e990f649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b58552ac6032830af905",
+    "url": "/static/css/main.134e1cde.chunk.css"
+  },
+  {
+    "revision": "7948bc4bd077cb94439f",
+    "url": "/static/js/2.6a91b009.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.6a91b009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b58552ac6032830af905",
+    "url": "/static/js/main.8744ab71.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "695611be64ffb7316ddae43e33adc06d",
+    "url": "/static/media/danang-1.695611be.jpg"
+  },
+  {
+    "revision": "e841c501c9f32655abc7fbc916685fbe",
+    "url": "/static/media/hoi-an.e841c501.jpeg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "18f54318d3635cb572b1c11bdbc5574d",
+    "url": "/static/media/portrait_1.18f54318.jpg"
+  }
+]);
